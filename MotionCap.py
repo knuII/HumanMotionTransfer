443de89555d8327adffd3eaf67b9f4918a2230c9
Shreyash <shreyashmohadikar@gmail.com>
2024-05-04 22:29:00 +0530

    new commit

---
+++ MotionCap.py
@@ -0,0 +1,29 @@
+##MotionCap.py
+
+import cv2
+from PoseModule import PoseDetector
+
+cap = cv2.VideoCapture('Video.mp4')
+
+detector = PoseDetector()
+posList = []
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    lmList, bboxInfo = detector.findPosition(img)
+
+    if bboxInfo:
+        lmString = ''
+        for lm in lmList:
+            lmString += f'{lm[0]},{img.shape[0] - lm[1]},{lm[2]},'
+        posList.append(lmString)
+
+    # print(len(posList))
+    # print(lmList)
+
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(1)
+
+    if key == ord('s'):
+        with open("AnimationFileNew.txt", 'w') as f:
+            f.writelines(["%s\n" % item for item in posList])
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+if bboxInfo:
+    #     lmString = ''
+    #     for lm in lmList:
+    #         lmString += f'{lm[0]},{img.shape[0] - lm[1]},{lm[2]},'
+    #     posList.append(lmString)
